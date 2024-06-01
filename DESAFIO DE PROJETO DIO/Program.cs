c64569426d4cf1d451587c76b9bace157cc61c74
Gustavo Ryan Joaquina <gugagg002@gmail.com>
2024-06-01 14:45:30 -0300

    first commit

---
+++ DESAFIO DE PROJETO DIO/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DESAFIO_DE_PROJETO_DIO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
